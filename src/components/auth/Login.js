f4aa26e17a03a2c71fbc890b48fc5e0387498ff0
jkuns76 <jkoonman@gmail.com>
2021-12-17 18:17:06 -0600

    got the mysite working

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -32,53 +32,55 @@ const Login = () => {
     }
 
     const handleUserInput = (event) => {
-        const copy = {...credentials}
+        const copy = { ...credentials }
         copy[event.target.id] = event.target.value
         syncAuth(copy)
     }
 
     return (
-        <main className="container--login">
-            <section>
-                <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Welcome to InTents</h1>
-                    <h2 className="h3 mb-3 font-weight-normal">Please sign in</h2>
-                    <fieldset>
-                        <label htmlFor="inputName"> User Name </label>
-                        <input type="name" onChange={handleUserInput}
-                            id="name"
-                            className="login-input"
-                            placeholder="Enter Your User Name"
-                            required autoFocus />
-                    </fieldset>
-                    <fieldset>
-                        <input
-                            onChange={
-                                (event) => {
-                                    const copy = {...credentials}
-                                    if (event.target.value === "on") {
-                                        copy.remember = true
+        <main className="main--container">
+            <div className="container--login">
+                <section>
+                    <form className="form--login" onSubmit={handleLogin}>
+                        <h1>Welcome to InTents</h1>
+                        <h2 className="h3 mb-3 font-weight-normal">Please sign in</h2>
+                        <fieldset>
+                            <label htmlFor="inputName"> User Name </label>
+                            <input type="name" onChange={handleUserInput}
+                                id="name"
+                                className="login-input"
+                                placeholder="Enter Your User Name"
+                                required autoFocus />
+                        </fieldset>
+                        <fieldset>
+                            <input
+                                onChange={
+                                    (event) => {
+                                        const copy = { ...credentials }
+                                        if (event.target.value === "on") {
+                                            copy.remember = true
+                                        }
+                                        else {
+                                            copy.remember = false
+                                        }
+                                        syncAuth(copy)
                                     }
-                                    else {
-                                        copy.remember = false
-                                    }
-                                    syncAuth(copy)
                                 }
-                            }
-                            defaultChecked={credentials.remember}
-                            type="checkbox" name="remember" id="remember" />
-                        <label htmlFor="remember"> Remember Me </label>
-                    </fieldset>
-                    <fieldset>
-                        <button className="btn-signIn"type="submit">
-                            Sign in
-                    </button>
-                    </fieldset>
-                </form>
-            </section>
-            <section className="link--register">
-                <Link to="/register">Not a member yet?</Link>
-            </section>
+                                defaultChecked={credentials.remember}
+                                type="checkbox" name="remember" id="remember" />
+                            <label htmlFor="remember"> Remember Me </label>
+                        </fieldset>
+                        <fieldset>
+                            <button className="btn-signIn" type="submit">
+                                Sign in
+                            </button>
+                        </fieldset>
+                    </form>
+                </section>
+                <section className="link--register">
+                    <Link to="/register">Not a member yet?</Link>
+                </section>
+            </div>
         </main>
     )
 }
--- src/components/camp/Campsite.js
+++ src/components/camp/Campsite.js
@@ -16,14 +16,6 @@ export const Campsite = () => {
             </div>
             <div>
                 {CampsiteList()}
-                {/* <div>
-                    <button onClick={evt => {
-                        evt.preventDefault()
-                        history.push("/campsites/mysites")
-                    }}
-                        className="btn-MySites">My Created Sites</button>
-                </div> */}
-
             </div>
 
         </>
